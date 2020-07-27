4290b7f6dd985619e49480d9d56267dbb7f36dcd
nguyenhoa <hoanguyen.179per2@gmail.com>
2020-07-27 23:32:44 +0700

    env variables 2 [springfox]

--- src/main/java/shift/management/ShiftManagementApplication.java
+++ src/main/java/shift/management/ShiftManagementApplication.java
@@ -24,7 +24,7 @@ import static springfox.documentation.builders.PathSelectors.regex;
 
 @SpringBootApplication
 @EntityScan("shift.management.entity")
-@EnableAutoConfiguration
+//@EnableAutoConfiguration
 @EnableSwagger2
 public class ShiftManagementApplication {
 	public static final Logger logger = Logger.getLogger(ShiftManagementApplication.class);
