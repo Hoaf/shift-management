3f8c6b955e5c528a990bafdaed47502275fd4bdc
nguyenhoa <hoanguyen.179per2@gmail.com>
2021-03-13 21:05:45 +0700

    product

--- src/main/java/shift/management/entity/Product.java
+++ src/main/java/shift/management/entity/Product.java
@@ -3,10 +3,15 @@ package shift.management.entity;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @Data
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String description;
--- src/main/java/shift/management/repository/ProductRepository.java
+++ src/main/java/shift/management/repository/ProductRepository.java
@@ -1,7 +1,9 @@
 package shift.management.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import shift.management.entity.Product;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
