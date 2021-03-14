3f7e7e4d536bdd7cc3fdcd47c698b347973157cd
Hoaf <hoanguyen.179per2@gmail.com>
2021-03-15 00:11:14 +0700

    origin

--- src/main/java/shift/management/controller/ProductController.java
+++ src/main/java/shift/management/controller/ProductController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(URL.API)
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ProductController {
     @Autowired
     private ProductService productService;
